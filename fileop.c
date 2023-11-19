4db9217462b4c304bfae6472feb671cdbe0d70ae
tamirwaleed <hero2000.TS@gmail.com>
2023-11-19 11:29:19 -0800

    teemo

---
+++ fileop.c
@@ -0,0 +1,140 @@
+#include "main.h"
+/**
+ * openf - opens file
+ * @file_name: file name
+ * Return: Void
+ */
+void openf(char *file_name)
+{
+FILE *fod = fopen(file_name, "r");
+if (file_name == NULL || fod == NULL)
+err(2, file_name);
+readf(fod);
+fclose(fod);
+}
+/**
+ * readf - reads file
+ * @fod: file pointer
+ * Return: Void
+ */
+void readf(FILE *fod);
+{
+int lineno, fmt = 0;
+char *buffer = NULL;
+size_t lens = 0;
+for (lineno = 1; getline(&buffer, &lens, fod) != -1; lineno++)
+fmt = parseln(buffer, lineno, fmt);
+free(buffer);
+}
+/**
+ * findfct - find the appropriate function for the opcode
+ * @opcode: opcode
+ * @val: argument of opcode
+ * @fmt:  storage format. 0 - Stack, 1 - Queue.
+ * @line: line number
+ * Return: void
+ */
+void findfct(char *opcode, char *val, int fmt, int line)
+{
+int i;
+int flag;
+instruction_t func_list[] = {
+{"push", add_to_stack},
+{"pall", print_stack},
+{"pint", print_top},
+{"pop", pop_top},
+{"nop", nop},
+{"swap", swap_nodes},
+{"add", add_nodes},
+{"sub", sub_nodes},
+{"div", div_nodes},
+{"mul", mul_nodes},
+{"mod", mod_nodes},
+{"pchar", print_char},
+{"pstr", print_str},
+{"rotl", rotl},
+{"rotr", rotr},
+{NULL, NULL}
+};
+if (opcode[0] == '#')
+return;
+for (flag = 1, i = 0; func_list[i].opcode != NULL; i++)
+{
+if (strcmp(opcode, func_list[i].opcode) == 0)
+{
+call_fun(func_list[i].f, opcode, val, line, fmt);
+flag = 0;
+}
+}
+if (flag == 1)
+err(3, line, opcode);
+}
+/**
+ * parseln - Separates each line into tokens to determine
+ * which function to call
+ * @buff: line from the file
+ * @lineno: line number
+ * @fmt:  storage format. If 0 Nodes will be entered as a stack.
+ * if 1 nodes will be entered as a queue.
+ * Return: Returns 0 if the opcode is stack. 1 if queue.
+ */
+int parseln(char *buff, int lineno, int fmt)
+{
+char *opcode, *value;
+	const char *delim = "\n ";
+
+	if (buff == NULL)
+		err(4);
+
+	opcode = strtok(buff, delim);
+	if (opcode == NULL)
+		return (format);
+	value = strtok(NULL, delim);
+
+	if (strcmp(opcode, "stack") == 0)
+		return (0);
+	if (strcmp(opcode, "queue") == 0)
+		return (1);
+
+	findfct(opcode, value, lineno, fmt);
+	return (fmt);
+}
+/**
+ * call_fun - Calls the required function.
+ * @func: Pointer to the function that is about to be called.
+ * @op: string representing the opcode.
+ * @val: string representing a numeric value.
+ * @ln: line numeber for the instruction.
+ * @format: Format specifier. If 0 Nodes will be entered as a stack.
+ * if 1 nodes will be entered as a queue.
+ */
+void call_fun(op_func func, char *op, char *val, int ln, int format)
+{
+	stack_t *node;
+	int flag;
+	int i;
+
+	flag = 1;
+	if (strcmp(op, "push") == 0)
+	{
+		if (val != NULL && val[0] == '-')
+		{
+			val = val + 1;
+			flag = -1;
+		}
+		if (val == NULL)
+			err(5, ln);
+		for (i = 0; val[i] != '\0'; i++)
+		{
+			if (isdigit(val[i]) == 0)
+				err(5, ln);
+		}
+		node = create_node(atoi(val) * flag);
+		if (format == 0)
+			func(&node, ln);
+		if (format == 1)
+			add_to_queue(&node, ln);
+	}
+	else
+		func(&top, ln);
+}
--- main.h
+++ main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <ctype.h>
+#define _GNU_SOURCE
+#include <stdlib.h>
+#include <unistd.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -13,9 +20,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+int n;
+struct stack_s *prev;
+struct stack_s *next;
 } stack_t;
 /**
  * struct instruction_s - opcode and its function
@@ -27,8 +34,39 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+char *opcode;
+void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+extern stack_t *top;
+typedef void (*op_fct)(stack_t **, unsigned int);
+
+void openf(char *file_name);
+void readf(FILE *);
+void findfct(char *, char *, int, int);
+int parseln(char *buffer, int line_number, int format);
+int lenchrs(FILE *);
+void call_fun(op_func func, char *op, char *val, int ln, int format);
+stack_t *create_node(int n);
+void freenodes(void);
+void printstack(stack_t **, unsigned int);
+void addtostack(stack_t **, unsigned int);
+void addtoqueue(stack_t **, unsigned int);
+void printtop(stack_t **, unsigned int);
+void poptop(stack_t **, unsigned int);
+void nop(stack_t **, unsigned int);
+void swapnodes(stack_t **, unsigned int);
+void addnodes(stack_t **, unsigned int);
+void subnodes(stack_t **, unsigned int);
+void divnodes(stack_t **, unsigned int);
+void mulnodes(stack_t **, unsigned int);
+void modnodes(stack_t **, unsigned int);
+void _printchar(stack_t **, unsigned int);
+void _printstr(stack_t **, unsigned int);
+void _rotl(stack_t **, unsigned int);
+void _rotr(stack_t **, unsigned int);
+void err(int error_code, ...);
+void moreerr(int error_code, ...);
+void stringerr(int error_code, ...);
 
 #endif
